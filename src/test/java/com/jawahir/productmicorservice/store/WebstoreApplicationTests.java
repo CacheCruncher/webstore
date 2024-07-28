f8487b208df559443e2f61b722d28b286434c3c0
CacheCruncher <jawahir.abul@gmail.com>
2024-07-28 09:13:42 +0400

    init

---
+++ src/test/java/com/jawahir/productmicorservice/store/WebstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jawahir.productmicorservice.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
